10bb525c841eed8a6bc1e7bfcffeaaa859a6789b
Dávid Jakab <david.jakab1@stud.uni-corvinus.hu>
2021-10-29 00:41:02 +0200

    kész

--- IRF_6/IRF_6/Form1.cs
+++ IRF_6/IRF_6/Form1.cs
@@ -17,10 +17,33 @@ namespace IRF_6
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         
         public Form1()
         {
             InitializeComponent();
+
+            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+            GetCurrenciesRequestBody request = new GetCurrenciesRequestBody();
+            var response = mnbService.GetCurrencies(request);
+            var result = response.GetCurrenciesResult;
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement item in xml.DocumentElement.ChildNodes[0])
+            {
+                string newItem = item.InnerText;
+                Currencies.Add(newItem);
+            }
+            comboBox1.DataSource = Currencies;
+
+            RefreshData();
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+
+            comboBox1.DataSource = Currencies;
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -71,6 +94,21 @@ namespace IRF_6
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
