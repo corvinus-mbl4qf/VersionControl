9808e7739f812d3c0e34dc16a7822864d3c297de
Dávid Jakab <david.jakab1@stud.uni-corvinus.hu>
2021-11-09 13:32:12 +0100

    1-2 feladat

---
+++ IRF_7/IRF_7/Form1.cs
@@ -0,0 +1,59 @@
+﻿using IRF_7.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IRF_7
+{
+    public partial class Form1 : Form
+    {
+        List<Ball> _balls = new List<Ball>();
+
+        BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var maxPosition = 0;
+
+            foreach (Ball ball in _balls)
+            {
+                ball.MoveBall();
+                if (ball.Left > maxPosition)
+                    maxPosition = ball.Left;
+            }
+
+            if (maxPosition > 1000)
+            {
+                var oldestBall = _balls[0];
+                mainPanel.Controls.Remove(oldestBall);
+                _balls.Remove(oldestBall);
+            }
+        }
+    }
+}
