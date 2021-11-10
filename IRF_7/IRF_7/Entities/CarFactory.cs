62e181a5bfc609bebeb5bdb63eff50916aef4084
Dávid Jakab <david.jakab1@stud.uni-corvinus.hu>
2021-11-10 13:25:26 +0100

    valamelyik feladat

--- IRF_7/IRF_7/Entities/CarFactory.cs
+++ IRF_7/IRF_7/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace IRF_7.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
