41aaf2c1636c7969878948d7ad0d444fbe38cdb3
Dávid Jakab <david.jakab1@stud.uni-corvinus.hu>
2021-12-01 12:14:42 +0100

    osztályok létrehozása

---
+++ IRF_9/IRF_9/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_9.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NumberOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
